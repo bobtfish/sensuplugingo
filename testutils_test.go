d22d0a9a24252ba277d06c53a408ea428b65056f
Tomas Doran <bobtfish@bobtfish.net>
2014-08-13 22:28:11 +0000

    Test the error case too

--- testutils_test.go
+++ testutils_test.go
@@ -17,3 +17,13 @@ func Test_exampleData(t *testing.T) {
 		t.Error(fmt.Sprintf("bad example data: expected %d, got %d - %s", expLen, len(st), st))
 	}
 }
+
+func Test_exampleDataFileNotFound(t *testing.T) {
+        _, err := exampleData("doesnotexist")
+        if err != nil {
+                t.Log("error reading non existant file")
+        } else {
+		t.Error("Didn't report an error")
+	}
+}
+
