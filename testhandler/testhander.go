4e0a513258aa20cce43d8bbe78779f17508b8245
Tomas Doran <bobtfish@bobtfish.net>
2014-08-19 02:41:01 +0000

    Basic structure for a handler which may do something

--- testhandler/testhander.go
+++ testhandler/testhander.go
@@ -3,12 +3,31 @@ package main
 import (
 	"fmt"
 	"github.com/bobtfish/sensuplugingo/handler"
+	"github.com/bobtfish/sensuplugingo"
 )
+
+func FormatMessage(e sensuplugingo.Event) string {
+	return("foo")
+}
+
+func GetChannels() (c []string) {
+	return c
+}
+
+func Send(channel string, message string) error {
+	// system('/nail/sys/bin/nodebot', channel, message)
+	return(nil)
+}
 
 func main() {
 	event, err := handler.GetEventFromStdin()
 	if err != nil {
 		panic("Error getting Event")
+	}
+	message := FormatMessage(event)
+	channels := GetChannels()
+	for _, channel := range channels {
+		Send(channel, message)	
 	}
 	fmt.Println(event)
 }
